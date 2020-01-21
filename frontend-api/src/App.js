e05512f665a35867df98e9a2b1cf986e0858f577
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-21 15:33:04 -0500

    setting up api via fetch call

--- frontend-api/src/App.js
+++ frontend-api/src/App.js
@@ -1,18 +1,47 @@
-import React from 'react';
-import Home from './Components/Home/Home'
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Movie from "./Components/Movie/Movie";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Home />
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    const url = "https://movie-express-custom-api.herokuapp.com/"
+
+    fetch(url)
+     .then(response => response.json())
+     .then(response => {
+      console.log(response)
+    })
+
+  }
+  render() {
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          </header>
+          <main>
+          <Route path="/" exact component={Home} />
+          <Route path="/:title" render={routerProps => (
+            <Movie 
+            {...routerProps} 
+            {...this.state} />
+          )} />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
