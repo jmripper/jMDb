08ccbf73b8a811d29e5797fa2a028b273b6eac6f
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-21 15:06:55 -0500

    set up movie and home components and installed react-router-dom

---
+++ frontend-api/src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+    return (
+      <>
+          <p>
+            HOME
+          </p>
+      </>
+    );
+  }
+  
+  export default Home;
+  
---
+++ frontend-api/src/Components/Movie/Movie.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Movie() {
+    return (
+      <>
+          <p>
+            MOVIE
+          </p>
+      </>
+    );
+  }
+  
+  export default Movie;
