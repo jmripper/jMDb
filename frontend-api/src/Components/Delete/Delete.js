b530bf0d1219d465b054911b8979929379b619b1
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-27 21:30:53 -0500

    updated form

--- frontend-api/src/Components/Delete/Delete.js
+++ frontend-api/src/Components/Delete/Delete.js
@@ -1,14 +1,22 @@
 import React from 'react';
-
+import axios from 'axios';
 
 const Delete = props => { 
+    const url = "https://movie-express-custom-api.herokuapp.com/delete" + props.movie._id
+
+    axios.delete(url)
+    .then(response => {
+        console.log(response)
+    })
+    .catch(err => console.log(err));
+
     return (
       <>
-        <div className="list-wrapper">
+        <div className="delete">
         </div>
       </>
     );
   };
   
-  export default Delete;
+export default Delete;
   
--- frontend-api/src/Components/Update/Update.js
+++ frontend-api/src/Components/Update/Update.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Update.css";
 import axios from "axios";
 
 class Update extends Component {
@@ -20,41 +21,50 @@ class Update extends Component {
 
   handleSubmit(evt) {
     evt.preventDefault();
+    const url =
+      "https://movie-express-custom-api.herokuapp.com/update/" +
+      this.props.params.match._id;
+    axios
+      .put(url, {
+        year: this.state.year,
+        rated: this.state.rated
+      })
+      .then(response => {
+        console.log(response);
+        console.log(response.data);
+        alert("movie was updated!");
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
-    const {
-      year,
-      rated,
-    } = this.state;
-    console.log(this.state.movie);
+    const { year, rated, movie } = this.state;
     return (
       <>
-        <form className="create-form" onSubmit={this.handleSubmit}>
-          <label htmlFor="year">Year:</label>
-          <input
-            type="text"
-            name="year"
-            placeholder="2020"
-            value={year}
-            required
-            onChange={this.handleChange}
-          />
-          <label htmlFor="title">Rating:</label>
-          <input
-            type="text"
-            name="rated"
-            placeholder=""
-            value={rated}
-            required
-            onChange={this.handleChange}
-          />
-          <input
-            className="submit-button"
-            type="submit"
-            value="Update"
-          />
-        </form>
+        <div className="update-container">
+          <h3>Update {movie.title}'s Information:</h3>
+          <form className="update-form" onSubmit={this.handleSubmit}>
+            <label htmlFor="year">Year:</label>
+            <input
+              type="text"
+              name="year"
+              placeholder="2020"
+              value={year}
+              required
+              onChange={this.handleChange}
+            />
+            <label htmlFor="title">Rating:</label>
+            <input
+              type="text"
+              name="rated"
+              placeholder=""
+              value={rated}
+              required
+              onChange={this.handleChange}
+            />
+            <input className="submit-button" type="submit" value="Update" />
+          </form>
+        </div>
       </>
     );
   }
